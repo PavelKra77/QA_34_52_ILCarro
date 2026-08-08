cbe95630ffeaba396e8f8f7d3a5997818cb1cb8f
PavelKra77 <pav.kravets86@gmail.com>
2026-08-08 20:52:41 +0300

    Start project

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,4 +1,28 @@
 package ui_tests;
 
-public class LoginTests {
+import dto.UserLombok;
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends AppManager {
+    @BeforeMethod
+    public void goToLoginPage(){
+        new HomePage(getDriver()).clickBtnLogin();
+    }
+
+
+    @Test
+    public void loginPositiveTest(){
+        UserLombok user = UserLombok.builder()
+                .username("pav.kravets86@gmail.com")
+                .password("Absent45!")
+                .build();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm(user);
+        loginPage.clickBtnYalla();
+
+    }
 }
