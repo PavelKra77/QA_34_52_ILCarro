f0a0c499836948e8b1e21e766ff5283ecfe4f3ba
PavelKra77 <pav.kravets86@gmail.com>
2026-09-21 17:15:06 +0300

    Task

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -44,7 +44,7 @@ public class AppManager {
         }
 
         driver.manage().window().maximize();
-        //driver.manage().window().setSize(new Dimension(1920, 1080));
+        //driver.manage().window().setSize(new Dimension(1920, 1080)); k,knk
         logger.info("Start testing with method -->" + method.getName());
         WebDriverListener webDriverListener = new WDListener();
         driver = new EventFiringDecorator<>(webDriverListener).decorate(driver);
