821a5cb765ad5f9471fc607d42a0fdddfae0fa99
PavelKra77 <pav.kravets86@gmail.com>
2026-09-21 19:52:54 +0300

    Task

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -63,7 +63,7 @@ public abstract class BasePage {
     }
 
     public void clickWait(WebElement element){
-        new WebDriverWait(driver, Duration.ofSeconds(5))
+        new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions
                         .elementToBeClickable(element)).click();}
 
